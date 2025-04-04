23d5fb8b679fe2cb4f14f89d20576f61ab1eaf03
Wojdylak <karolwojdyla37@gmail.com>
2025-04-04 15:14:20 +0200

    [Maintenance] Add and configure SyliusLabs ECS rules

--- ecs.php
+++ ecs.php
@@ -2,17 +2,45 @@
 
 declare(strict_types=1);
 
+use PhpCsFixer\Fixer\ClassNotation\OrderedTypesFixer;
+use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
+use PhpCsFixer\Fixer\FunctionNotation\NullableTypeDeclarationForDefaultNullValueFixer;
+use PhpCsFixer\Fixer\Phpdoc\PhpdocSeparationFixer;
+use SlevomatCodingStandard\Sniffs\Commenting\InlineDocCommentDeclarationSniff;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return static function (ECSConfig $config): void {
-    $config->import('vendor/bitbag/coding-standard/ecs.php');
+    $config->import('vendor/sylius-labs/coding-standard/ecs.php');
 
+    $config->parallel();
     $config->paths([
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
+        'src/',
+        'spec/',
+        'tests/Behat/',
+        'tests/Functional/',
+        'tests/Integration/',
     ]);
     $config->skip([
+        InlineDocCommentDeclarationSniff::class . '.MissingVariable',
+        InlineDocCommentDeclarationSniff::class . '.NoAssignment',
         '**/var/*',
-        '**/node_modules/*',
     ]);
+    $config->ruleWithConfiguration(PhpdocSeparationFixer::class, ['groups' => [['Given', 'When', 'Then']]]);
+    $config->ruleWithConfiguration(OrderedTypesFixer::class, ['null_adjustment' => 'always_last']);
+    $config->ruleWithConfiguration(NullableTypeDeclarationForDefaultNullValueFixer::class, ['use_nullable_type_declaration' => true]);
+    $config->ruleWithConfiguration(
+        HeaderCommentFixer::class,
+        [
+            'location' => 'after_open',
+            'comment_type' => HeaderCommentFixer::HEADER_COMMENT,
+            'header' => <<<TEXT
+This file is part of the Sylius Cms Plugin package.
+
+(c) Sylius Sp. z o.o.
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+TEXT
+        ]
+    );
 };
