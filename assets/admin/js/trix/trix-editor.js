ac911cad73d0bc14a3595fedac7deaf5fd293e81
Jan Goralski <jan.wojciech.goralski@gmail.com>
2025-05-02 15:56:45 +0200

    [Admin] Make trix usable in live components

--- assets/admin/js/trix/trix-editor.js
+++ assets/admin/js/trix/trix-editor.js
@@ -1,2 +1,20 @@
-import 'trix';
+import Trix from 'trix';
 import 'trix/dist/trix.css';
+
+document.addEventListener('trix-before-initialize', updateToolbars);
+
+document.addEventListener('trix-blur', (event) => {
+    const innerInput = document.getElementById(event.target.attributes.input.value);
+
+    if (innerInput) {
+        innerInput.dispatchEvent(new Event('change', { bubbles: true }));
+        updateToolbars();
+    }
+});
+
+function updateToolbars(event) {
+    const toolbars = document.querySelectorAll('trix-toolbar');
+    const html = Trix.config.toolbar.getDefaultHTML();
+
+    toolbars.forEach((toolbar) => (toolbar.innerHTML = html));
+}
