ed52ad80dc9cd3f638b9c3d814f28cf10537b13a
Rafikooo <thecrazyrafik@gmail.com>
2025-05-12 09:17:29 +0200

    WIP

---
+++ src/Plugin/Installer/CmsPluginInstaller.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+
+namespace Sylius\CmsPlugin\Plugin\Installer;
+
+use App\Plugin\Installer\PluginInstallerInterface;
+use Symfony\Component\Process\Process;
+
+class CmsPluginInstaller implements PluginInstallerInterface
+{
+    public function supports(string $packageName): bool
+    {
+        return $packageName === 'sylius/cms-plugin';
+    }
+
+    public function install(string $version): void
+    {
+        Process::fromShellCommandline('yarn add trix@^2.0.0 swiper@^11.2.6')->run();
+    }
+}
