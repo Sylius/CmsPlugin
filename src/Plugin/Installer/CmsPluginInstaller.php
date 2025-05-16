211e445092e446c0a17414c78de1a51dd0f01f80
Rafikooo <thecrazyrafik@gmail.com>
2025-05-16 10:34:52 +0200

    WIP

--- src/Plugin/Installer/CmsPluginInstaller.php
+++ src/Plugin/Installer/CmsPluginInstaller.php
@@ -6,6 +6,7 @@ declare(strict_types=1);
 namespace Sylius\CmsPlugin\Plugin\Installer;
 
 use App\Plugin\Installer\PluginInstallerInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
 use Symfony\Component\Process\Process;
 
@@ -17,9 +18,33 @@ class CmsPluginInstaller implements PluginInstallerInterface
         return $packageName === 'sylius/cms-plugin';
     }
 
-    public function install(string $version): void
+    public function install(string $version, $input, $output): void
     {
+        $io = new SymfonyStyle($input, $output);
+
+        $io->section('Installing CMS Plugin');
+        $process = Process::fromShellCommandline('cat package.json');
+        $exitCode = $process->run();
+        if ($exitCode === 0) {
+            echo $process->getOutput();
+        } else {
+            echo "Błąd: ", $process->getErrorOutput();
+        }
+
+
+
+
         Process::fromShellCommandline('php bin/console sylius:fixtures:load cms -n')->run();
         Process::fromShellCommandline('yarn add trix@^2.0.0 swiper@^11.2.6')->run();
+
+        $io->success('Po dodaniu trixa');
+        $process = Process::fromShellCommandline('cat package.json');
+        $exitCode = $process->run();
+        if ($exitCode === 0) {
+            echo $process->getOutput();
+        } else {
+            echo "Błąd: ", $process->getErrorOutput();
+        }
+
     }
 }
