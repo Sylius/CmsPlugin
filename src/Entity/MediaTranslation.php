ca557081a56dc33b07415b9477d367acb4de00b2
Norbert Glanc <norglanc@gmail.com>
2025-07-10 13:02:13 +0200

    Update search media fields

--- src/Entity/MediaTranslation.php
+++ src/Entity/MediaTranslation.php
@@ -25,8 +25,6 @@ class MediaTranslation extends AbstractTranslation implements MediaTranslationIn
 
     protected ?string $link = null;
 
-    protected ?string $name = null;
-
     public function getId(): ?int
     {
         return $this->id;
@@ -60,15 +58,5 @@ class MediaTranslation extends AbstractTranslation implements MediaTranslationIn
     public function setLink(?string $link): void
     {
         $this->link = $link;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(?string $name): void
-    {
-        $this->name = $name;
     }
 }
