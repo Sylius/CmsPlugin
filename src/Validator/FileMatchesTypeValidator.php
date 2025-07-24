0c2dbff0c6995068179e19b67ab28950f94f02c5
Norbert Glanc <norglanc@gmail.com>
2025-07-24 08:50:35 +0200

    Fix FileMatchesTypeValidator and fields order

--- src/Validator/FileMatchesTypeValidator.php
+++ src/Validator/FileMatchesTypeValidator.php
@@ -30,16 +30,13 @@ final class FileMatchesTypeValidator extends ConstraintValidator
             return;
         }
 
-        if ($value->hasFile() && null !== $value->getFile()->getMimeType()) {
-            return;
-        }
-
         $mime = $value->hasFile() ? $value->getFile()->getMimeType() : $value->getMimeType();
 
         if (MediaInterface::IMAGE_TYPE === $value->getType() && !(str_starts_with($mime, 'image/'))) {
             $this->context->buildViolation($constraint->messageImage)
+                ->atPath($constraint->field)
                 ->addViolation()
-            ;
+             ;
         }
 
         if (MediaInterface::VIDEO_TYPE === $value->getType() && !(str_starts_with($mime, 'video/'))) {
